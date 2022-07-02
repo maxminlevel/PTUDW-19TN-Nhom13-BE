a853e4bfba507bb962e19cfff079d37b8438e4a8
maxminlevel <toanvo@peahoki.net>
2022-07-02 18:04:57 +0700

    fix minor and add cmt

--- src/api.js
+++ src/api.js
@@ -133,7 +133,7 @@ const initEngineView = (ctx) => {
   app.engine('hbs', hbs.engine)
   app.set('view engine', 'hbs')
   app.set('views', path.join(__dirname, 'views'))
-  app.use(express.static(path.join(__dirname, '@/public')))
+  app.use(express.static(path.join(__dirname, './public')))
 
   i18n.configure({
     locales: ['vi', 'en'],
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,16 +3,22 @@ const UserService = require('@/services/user.service')
 const {authUser, authAdmin} = require('@/hook/auth')
 const midleware = require('@/hook/middleware')
 
+// List all user in admin view
 router.get('/', authAdmin, midleware.page.request, async (req, res, next) => {
+  console.log('first')
   try {
     res.success(await UserService.list(req.ctx), {
       metadata: req.ctx.get('data.paging'),
     })
   } catch (e) {
+    console.log(e)
     res.fail(400, error.detail, error.statusCode)
   }
   next()
 })
+
+// Get detail user in admin view
+// Get detail in user profile
 router.get('/:id', authUser, async (req, res, next) => {
   try {
     const id = req.params.id
@@ -22,6 +28,8 @@ router.get('/:id', authUser, async (req, res, next) => {
   }
   next()
 })
+
+// Add new user in admin view
 router.post('/', authAdmin, async (req, res, next) => {
   try {
     res.success(await UserService.create(req.ctx, req.body))
@@ -31,6 +39,8 @@ router.post('/', authAdmin, async (req, res, next) => {
   }
   next()
 })
+
+// Modify user in admin view
 router.put('/:id', authUser, async (req, res, next) => {
   try {
     const id = req.params.id
@@ -40,6 +50,8 @@ router.put('/:id', authUser, async (req, res, next) => {
   }
   next()
 })
+
+// Soft delete user
 router.delete('/:id', authAdmin, async (req, res, next) => {
   try {
     const id = req.ctx.get('data.id')
@@ -49,7 +61,21 @@ router.delete('/:id', authAdmin, async (req, res, next) => {
   }
   next()
 })
+
+// Lock user with id
 router.post('/lock:id', authAdmin, async (req, res, next) => {
+  try {
+    const id = req.ctx.get('data.id')
+    const body = req.body
+    res.success(await UserService.update(req.ctx, id, body))
+  } catch (error) {
+    res.fail(400, error.detail, error.statusCode)
+  }
+  next()
+})
+
+// Unlock user with id
+router.post('/unlock:id', authAdmin, async (req, res, next) => {
   try {
     const id = req.ctx.get('data.id')
     const body = req.body
